b8e9864db3389a0c983bfb5eaea9b2fc67d49da0
Michał Pazio <elmiciu@gmail.com>
2024-02-26 18:41:29 +0100

    Add basic tests for argument checking

---
+++ DataGenerator/DataGenerator.Tests/ProgramArgumentsTests.cs
@@ -0,0 +1,31 @@
+﻿using DataGenerator.Utils;
+
+namespace DataGenerator.Tests
+{
+    public class ProgramArgumentsTests
+    {
+        [Fact]
+        public void RunAppWithNoArguments()
+        {
+            //Arrange
+
+            //Act
+            Action action = () => ArgumentChecker.CheckArguments(null!);
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void RunAppWithEmptyArgumentsList()
+        {
+            //Arrange
+
+            //Act
+            Action action = () => ArgumentChecker.CheckArguments(new string[] { });
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(action);
+        }
+    }
+}
---
+++ DataGenerator/DataGenerator/Utils/ArgumentChecker.cs
@@ -0,0 +1,11 @@
+﻿namespace DataGenerator.Utils
+{
+    public static class ArgumentChecker
+    {
+        public static void CheckArguments(string[] args)
+        {
+            if (args is null || args.Length < 1)
+                throw new ArgumentNullException(nameof(args));
+        }
+    }
+}
