78b405a58973df634f1d78d1fd3e7c77489ab2f7
Michał Pazio <elmiciu@gmail.com>
2024-02-28 16:38:26 +0100

    Add FileSaver

---
+++ DataGenerator/DataGenerator/Utils/FileSaver.cs
@@ -0,0 +1,11 @@
+﻿namespace DataGenerator.Utils
+{
+    public static class FileSaver
+    {
+        public static async Task Save(string[] querys, string database, string? path = null)
+        {
+            string pathWithFileName = $"{path ?? string.Empty}/{database}inserts.txt";
+            await File.WriteAllLinesAsync(pathWithFileName, querys);
+        }
+    }
+}
