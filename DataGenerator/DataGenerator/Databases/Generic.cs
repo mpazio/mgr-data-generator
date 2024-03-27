5ac0352b5113f11bbd7a6d13f2d06313cf5ade4c
Michał Pazio <elmiciu@gmail.com>
2024-03-27 17:14:37 +0100

    Implement generic Inserts generation

---
+++ DataGenerator/DataGenerator/Databases/Generic.cs
@@ -0,0 +1,17 @@
+﻿namespace DataGenerator.Databases;
+
+public class Generic : IDatabase
+{
+    public string[] GenerateInserts(string[] jsonValues)
+    {
+        string[] queries = new string[jsonValues.Length];
+        for (int i = 0; i < jsonValues.Length; i++)
+        {
+            var json = jsonValues[i];
+            string query = $"{json}";
+            queries[i] = query;
+        }
+
+        return queries;
+    }
+}
