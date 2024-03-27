7f3d9a7414eed7b6334a66538084175c9698222f
Michał Pazio <elmiciu@gmail.com>
2024-03-27 17:16:47 +0100

    Add Couchbase and CouchDb Insert generation

---
+++ DataGenerator/DataGenerator/Databases/CouchDb.cs
@@ -0,0 +1,9 @@
+﻿namespace DataGenerator.Databases;
+
+public class CouchDb : IDatabase
+{
+    public string[] GenerateInserts(string[] jsonValues)
+    {
+        return new Generic().GenerateInserts(jsonValues);
+    }
+}
---
+++ DataGenerator/DataGenerator/Databases/Couchbase.cs
@@ -0,0 +1,9 @@
+﻿namespace DataGenerator.Databases;
+
+public class Couchbase : IDatabase
+{
+    public string[] GenerateInserts(string[] jsonValues)
+    {
+        return new Generic().GenerateInserts(jsonValues);
+    }
+}
